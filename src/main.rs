ec7b2e810114e582f2c56f63699e8dd92ea3c181
begdany <begdany@yandex.ru>
2024-09-11 23:16:13 +0300

    Добавлена возможность выбора управления работы программой. Добавлена поддержка маршрутов /previous, /next, /first, /last

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@ use std::io::{Read, Write}; // Используются для чтения/за
 use std::net::TcpStream; // Используется для TCP-соединения с сервером
 use tokio_postgres::{NoTls, Error}; // Используется для работы с базой данных
 use std::env; // Модуль env применяется для настройки отображения сообщений логирования
-use rand::Rng;
+use rand::Rng; // Для генерации id
+use std::io; // Для ввода пользователя
 
 // Определяем структуру, содержащую данные о товаре
 #[derive(Serialize)]
@@ -34,48 +35,102 @@ async fn main() -> Result<(), Error> {
         }
     });
 
-    // Выполняем запрос к базе данных
-    let row = client
-        .query_one("SELECT brand, name, price FROM data WHERE id = $1", &[&1])
-        .await?;
+    loop {
 
+        // Создаем TCP-соединение с сервером
+        let mut stream = TcpStream::connect("127.0.0.1:3000").unwrap();
+        let request: String;
 
-    // Создаем экземпляр структуры
-    let data = ItemData {
-        brand: row.get("brand"),
-        name: row.get("name"),
-        price: row.get("price"),
-        id: rand::thread_rng().gen_range(0..255),
-    };
+        // Вывод сообщения управления работы программой
+        println!("Введите 'r' для отправки новых данных");
+        println!("        'p' для перехода к предыдущим данным");
+        println!("        'n' для перехода к следующим данным");
+        println!("        'l' для перехода к последним данным");
+        println!("Введите любые другие символы для выхода:");
 
-    // Сериализуем объект data в JSON
-    let json = serde_json::to_string(&data).unwrap();
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
 
-    // Создаем TCP-соединение с сервером
-    let mut stream = TcpStream::connect("127.0.0.1:3000").unwrap();
+        match input.trim() {
+            "r" => { // Повторяем выполнение программы
 
-    // Формируем HTTP-запрос
-    let request = format!(
-        "POST /post HTTP/1.1\r\n\
-         Host: 127.0.0.1:3000\r\n\
-         Content-Type: application/json\r\n\
-         Content-Length: {}\r\n\
-         Connection: close\r\n\r\n\
-         {}",
-        json.len(),
-        json
-    );
+                // Выполняем запрос к базе данных
+                let row = client
+                    .query_one("SELECT brand, name, price FROM data WHERE id = $1", &[&1])
+                    .await?;
 
-    // Отправляем запрос
-    stream.write_all(request.as_bytes()).unwrap();
-    log::info!("Отправлен запрос на сервер.");
+                // Создаем экземпляр структуры
+                let data = ItemData {
+                    brand: row.get("brand"),
+                    name: row.get("name"),
+                    price: row.get("price"),
+                    id: rand::thread_rng().gen_range(0..255),
+                };
 
-    // Читаем ответ
-    let mut response = String::new();
-    stream.read_to_string(&mut response).unwrap();
+                // Сериализуем объект data в JSON
+                let json = serde_json::to_string(&data).unwrap();
+            
+                // Формируем HTTP-запрос
+                request = format!(
+                    "POST /post HTTP/1.1\r\n\
+                    Host: 127.0.0.1:3000\r\n\
+                    Content-Type: application/json\r\n\
+                    Content-Length: {}\r\n\
+                    Connection: close\r\n\r\n\
+                    {}",
+                    json.len(),
+                    json
+                );
 
-    // Выводим ответ в консоль
-    log::info!("Получен ответ от сервера:\n{}", response);
+            }
+            "p" => {
+                request = format!(
+                    "GET /previous HTTP/1.1\r\n\
+                    Host: 127.0.0.1:3000\r\n\
+                    Connection: close\r\n\r\n\
+                    "
+                );               
+            }
+            "n" => {
+                request = format!(
+                    "GET /next HTTP/1.1\r\n\
+                    Host: 127.0.0.1:3000\r\n\
+                    Connection: close\r\n\r\n\
+                    "
+                );               
+            }
+            "f" => {
+                request = format!(
+                    "GET /first HTTP/1.1\r\n\
+                    Host: 127.0.0.1:3000\r\n\
+                    Connection: close\r\n\r\n\
+                    "
+                );               
+            }
+            "l" => {
+                request = format!(
+                    "GET /last HTTP/1.1\r\n\
+                    Host: 127.0.0.1:3000\r\n\
+                    Connection: close\r\n\r\n\
+                    "
+                );               
+            }
+            _ => {
+                break;
+            }
+        }
 
+        // Отправляем запрос
+        stream.write_all(request.as_bytes()).unwrap();
+        log::info!("Отправлен запрос на сервер.");
+
+        // Читаем ответ
+        let mut response = String::new();
+        stream.read_to_string(&mut response).unwrap();
+
+        // Выводим ответ в консоль
+        log::info!("Получен ответ от сервера:\n{}", response);       
+    }
+  
     Ok(())
 }
